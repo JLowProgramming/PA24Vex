a5a4617ab0da0fbc1371ed2812f3eb927e19d00a
Tortuga-73 <setheharrison@gmail.com>
2024-10-23 10:24:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from vex import *
 
 # Brain should be defined by default
 brain=Brain()
-
+digin = DigitalIn(brain.three_wire_port.a)
 # Robot configuration code
 claw_motor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
 arm_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
@@ -32,8 +32,8 @@ right_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 # Begin project code
 # Main Controller loop to set motors to controller axis postiions
 while True:
-    left_motor.set_velocity((controller_1.axis2.position() + controller_1.axis1.position()), PERCENT)
-    right_motor.set_velocity((controller_1.axis2.position() - controller_1.axis1.position()), PERCENT)
-    left_motor.spin(FORWARD)
-    right_motor.spin(FORWARD)
-    wait(5, MSEC)
+    left_motor.set_velocity((controller_1.axis4.position() + controller_1.axis3.position()), 50)
+    right_motor.set_velocity((controller_1.axis4.position() - controller_1.axis3.position()), 50)
+    # left_motor.spin(FORWARD)
+    # right_motor.spin(FORWARD)
+    # wait(5, MSEC)
